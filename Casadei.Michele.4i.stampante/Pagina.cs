fa283a061128d5d003ae8fe9e43c96a6600926ed
Michele Casadei <127590217+MicheleCasadei706@users.noreply.github.com>
2023-12-04 21:43:06 +0100

    Update Pagina.cs

--- Casadei.Michele.4i.stampante/Pagina.cs
+++ Casadei.Michele.4i.stampante/Pagina.cs
@@ -8,11 +8,13 @@ namespace Casadei.Michele._4i.stampante
 {
     public class Pagina
     {
+        //4 attributi CMYB che, se usata per stampare, consuma i serbatoi della stampante
         public int B { get; set; }
         public int Y { get; set; }
         public int M { get; set; }
         public int C { get; set; }
-
+        
+        //un costruttore che accetta colori specifici al massimo di valore 3
         public Pagina(int c, int m, int y, int b)
         {
             if (c > 3 || m > 3 || y > 3 || b > 3)
@@ -24,6 +26,7 @@ namespace Casadei.Michele._4i.stampante
             B = b;
         }
 
+        //un costruttore che crea una Pagina con colori random
         public Pagina()
         {
             var r = new Random();
